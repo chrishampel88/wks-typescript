99029c28496b9d7a26a83b345b22b7c7fb3cfc1e
Christian Hampel <christianhampel88@gmail.com>
2022-01-26 08:45:08 -0300

    terminado

--- clases/homeworks/ejercicio 4.ts
+++ clases/homeworks/ejercicio 4.ts
@@ -17,16 +17,19 @@ let franco: Persona = {
 }
 
 interface Estudiante extends Persona {
-    esActivo: boolean
+    esActivo?: boolean
 }
 
 let diego: Estudiante = {
     nombre: "Diego",
     edad: 29,
-    esActivo: false //que pasa si comento alguno?
+    //esActivo: false //que pasa si comento alguno?
+    //SE QUEJA XQ TE LO ESTA PIDIENDO OBLIGATORIAMENTE A MENOS Q USES ?
 }
 
 // Investiga la palabra reservada implements
+
+//LA NUEVA CLASE PUEDE TRATARSE CON LA MISMA FORMA PERO NO ES UN HIJO DE LA ANTERIOR
 
 // Existe otra alternativa para realizar lo mismo?
 // Si, types.
@@ -48,5 +51,12 @@ let Fede: Student = {
 }
 
 //Investiga sus diferencias
-
+//          Type	                                                Interface
+//Es una colección de tipos de datos.	                                 Es una forma de sintaxis.
+//Admite la creación de un nuevo nombre para un tipo.                   Proporciona una forma de definir las entidades.
+//Tiene comparativamente menos capacidades.	                            Tiene comparativamente más capacidades.
+//No admite el uso de un objeto.	                                    Es compatible con el uso de un objeto.
+//No se pueden usar varias declaraciones fusionadas.	                Se pueden usar múltiples declaraciones fusionadas.
+//Dos tipos que tienen el mismo nombre provocan una excepción.	        Dos interfaces que tienen el mismo nombre se fusionan.
+//No tiene propósitos de implementación.	                            Tiene un propósito de implementación.gi
 export {}
